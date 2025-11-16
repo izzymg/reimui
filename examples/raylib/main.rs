c41024999808cd221450fb45ac9d2997ab1563d4
izzymg <izzymg@proton.me>
2025-11-16 20:34:19 +1300

    Improve example

--- examples/raylib/main.rs
+++ examples/raylib/main.rs
@@ -2,9 +2,7 @@ use std::ffi::CString;
 
 use raylib::prelude::*;
 
-use reimui::{
-    ButtonState, DrawCommand, FontInformation, Layout, LayoutDirection, UIContext, Vec2,
-};
+use reimui::{ButtonState, DrawCommand, FontInformation, Layout, LayoutDirection, UIContext, Vec2};
 
 struct RaylibFontInfo {
     font_size: i32,
@@ -21,44 +19,50 @@ impl FontInformation for RaylibFontInfo {
     }
 }
 
+/// Reimui drawing
+fn draw_ui<'a>(rl: &RaylibHandle, font_info: &'a RaylibFontInfo) -> UIContext<'a> {
+    let mouse = rl.get_mouse_position();
+    let mouse_state = if rl.is_mouse_button_down(MouseButton::MOUSE_BUTTON_LEFT) {
+        ButtonState::Down
+    } else {
+        ButtonState::Up
+    };
+
+    let mut ctx = UIContext::new(
+        font_info,
+        Vec2 {
+            x: mouse.x.max(0.0) as u32,
+            y: mouse.y.max(0.0) as u32,
+        },
+        mouse_state,
+    );
+
+    let mut layout = Layout::new(
+        LayoutDirection::Vertical,
+        10,
+        Vec2 { x: 28, y: 28 },
+        Vec2 { x: 620, y: 360 },
+    );
+    ctx.draw_text_layout(&mut layout, "reimui + raylib".into());
+    ctx.draw_text_layout(&mut layout, "i love user interfaces".into());
+    let clicked = ctx.draw_button(&mut layout, Vec2 { x: 18, y: 12 }, "Click this".into());
+    if clicked {
+        ctx.draw_text_layout(&mut layout, "hell yeah".into());
+    }
+    ctx
+}
+
+/// Raylib window & loop, just draws our UI
 fn main() {
-    let (mut rl, thread) = raylib::init()
-        .size(640, 400)
-        .title("r e i m u i")
-        .build();
+    let (mut rl, thread) = raylib::init().size(640, 400).title("r e i m u i").build();
 
     let font_info = RaylibFontInfo { font_size: 22 };
 
     while !rl.window_should_close() {
-        let mouse = rl.get_mouse_position();
-        let mouse_state = if rl.is_mouse_button_down(MouseButton::MOUSE_BUTTON_LEFT) {
-            ButtonState::Down
-        } else {
-            ButtonState::Up
-        };
+        // draw ui
+        let mut ctx = draw_ui(&rl, &font_info);
 
-        let mut ctx = UIContext::new(
-            &font_info,
-            Vec2 {
-                x: mouse.x.max(0.0) as u32,
-                y: mouse.y.max(0.0) as u32,
-            },
-            mouse_state,
-        );
-
-        let mut layout = Layout::new(
-            LayoutDirection::Vertical,
-            10,
-            Vec2 { x: 28, y: 28 },
-            Vec2 { x: 620, y: 360 },
-        );
-        ctx.draw_text_layout(&mut layout, "reimui + raylib".to_string());
-        ctx.draw_text_layout(
-            &mut layout,
-            "Immediate mode UI rendering to raylib".to_string(),
-        );
-        let clicked = ctx.draw_button(&mut layout, Vec2 { x: 18, y: 12 }, "Click me".to_string());
-
+        // transfer all draw commands into raylib as our renderer
         let mut d = rl.begin_drawing(&thread);
         d.clear_background(Color::RAYWHITE);
 
@@ -74,16 +78,6 @@ fn main() {
                     );
                 }
             }
-        }
-
-        if clicked {
-            d.draw_text(
-                "Button pressed",
-                28,
-                (layout.top_left.y + 4) as i32,
-                font_info.font_size,
-                Color::DARKGREEN,
-            );
         }
     }
 }
